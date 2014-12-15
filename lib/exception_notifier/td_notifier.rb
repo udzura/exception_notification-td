c3ca8184285b5f64ae879d6cd3daa6f8680c090e
Uchio KONDO <udzura@udzura.jp>
2014-12-15 14:33:46 +0900

    Refine parameter contents

--- lib/exception_notifier/td_notifier.rb
+++ lib/exception_notifier/td_notifier.rb
@@ -5,27 +5,52 @@ require "td-logger"
 module ExceptionNotifier
   class TdNotifier
     def initialize(options)
-      @database   = options.delete(:database)
       @table_name = options.delete(:table_name)
-      raise "Please set database and table_name. options: #{options.inspect}" if !@database or !@table_name
+      raise "Please set table_name. options: #{options.inspect}" unless @table_name
 
-      TreasureData::Logger.open(@database, options)
+      unless defined? TreasureData::Logger::Agent::Rails
+        @database = options.delete(:database)
+        raise "Please set database. options: #{options.inspect}" unless @database
+        TreasureData::Logger.open(@database, options)
+      end
     end
-    attr_accessor :td
 
     def call(exception, options = {})
       TD.event.post(@table_name, exception_to_td_data(exception, options))
     end
 
+    private
+    def request_klass
+      @request_klass ||= if defined?(ActionDispatch::Request)
+                           ActionDispatch::Request
+                         else
+                           require 'rack/request'
+                           Rack::Request
+                         end
+    rescue LoadError, NameError
+      warn "ExceptionNotification::Td is designed to be used with Rack-based apps. Skip some of features."
+      nil
+    end
+
     def exception_to_td_data(exception, options)
+      backtrace = exception.backtrace ? exception.backtrace[0..9] : []
       params = {
-        class: exception.class,
+        class: exception.class.to_s,
         message: exception.message,
-        backtrace: exception.backtrace,
+        backtrace: backtrace,
         hostname: (Socket.gethostname rescue nil),
         environment: Rails.env,
       }
-      params.merge!(request_env: options[:env]) if options[:env]
+      if request_klass && options[:env]
+        request = request_klass.new(options[:env])
+        params.merge!(
+          method: request.request_method,
+          request_url: request.url,
+          cookies: request.cookies,
+          referer: request.referer,
+        )
+        params[:post_body] = request.body unless request.get?
+      end
       params
     end
   end
